c7ccaedf75e05f7e72600bb12e46c0fa5794f9b3
Valentino Cueva <cuevacespedesvalentino@gmail.com>
2025-08-02 14:19:53 -0500

    Agrege contenedor de tarea

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const urlSupabase="https://tisvyezlhgyoymdnosoe.supabase.co"
 // Crear cliente de Supabase usando la variable global del CDN
 const supabase = window.supabase.createClient(urlSupabase, claveSupabase);
 
-async function crearTarea(titulo, descripcion = null, fecha_limite = null) {
+async function crearTarea(titulo, descripcion = "Descripción no especificada.", fecha_limite = null) {
   const { data, error } = await supabase
     .from("tareas")
     .insert([{ titulo, descripcion, fecha_limite }]);
@@ -20,13 +20,25 @@ async function crearTarea(titulo, descripcion = null, fecha_limite = null) {
   return data;
 }
 
+async function obtenerTareas() {
+  const { data, error } = await supabase.from("tareas").select("*");
+  if (error) {
+    console.error("Error al obtener las tareas: " + error.message);
+    return [];
+  } else {
+    console.log("Tareas obtenidas exitosamente" + data);
+    return data;
+  }
+}
+
 async function configurarFormulario() {
   const formulario = document.getElementById("formulario")
   formulario.onsubmit = async function (e) {
-    const titulo= formulario.titulo.value
-    const descripcion= formulario.descripcion.value
-    const fecha_limite= formulario.fecha_limite.value
-    crearTarea(titulo, descripcion, fecha_limite)
+    e.preventDefault();
+    const titulo = formulario.elements["titulo"].value;
+    const descripcion = formulario.elements["descripcion"].value;
+    const fecha_limite = formulario.elements["fecha_limite"].value;
+    crearTarea(titulo, descripcion, fecha_limite);
   }
   
 
@@ -35,6 +47,14 @@ async function configurarFormulario() {
 
 }
 
+function mostrarTareas(tareas) {
+  const contenedorTareas = document.getElementById
+  ("lista-tareas");
+  listaTareas.innerHTML = ""; // Limpiar la lista antes de mostrar las ta
+}
+
+
+
 // Ejecutar al cargar la página
 window.addEventListener("DOMContentLoaded", async () => {
   if (typeof window.supabase == "undefined") {
@@ -42,6 +62,8 @@ window.addEventListener("DOMContentLoaded", async () => {
     return;
   } else
   {console.log("supabase cargado exitosamente")}
-configurarFormulario();
-  console.log("Prueba Valentino")
+
+  configurarFormulario();
+
+  const tareas = obtenerTareas();
 });
